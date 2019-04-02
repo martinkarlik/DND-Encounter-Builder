c5cb25d209c7244f0d5cdda3b572bf91b7cc0388
martinkarlik <mkarli18@student.aau.dk>
2019-04-02 18:00:06 +0200

    Merge branch 'master' of https://github.com/VictorBuch/DMMiniproject
    # Conflicts:
    #	src/API.java

--- src/MarinAndVictorMP/API.java
+++ src/MarinAndVictorMP/API.java
@@ -1,4 +1,4 @@
-package MarinAndVictorMP;
+package src.MarinAndVictorMP;
 
 import netscape.javascript.JSObject;
 import java.net.*;
@@ -10,7 +10,7 @@ public class API {
 
     public static void main(String[] args) throws IOException {
         URL url = new URL("http://dnd5eapi.co/api/");
-        String query = "Monsters/268";
+        String query = "Monsters/325";
 
 
         URL urlQuery = new URL(url + query);
@@ -18,7 +18,7 @@ public class API {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(urlc.getInputStream()));
         String l;
-        while ((l=br.readLine())!=null){
+        while ((l = br.readLine()) != null){
             System.out.println(l);
 
         }
@@ -27,9 +27,3 @@ public class API {
     }
 }
 
-<<<<<<< HEAD:src/API.java
-
-=======
-//adding a comment
-// adding another comment
->>>>>>> e4d3808b3916f9ff19a22824986e09e44c8eca48:src/MarinAndVictorMP/API.java
--- src/MarinAndVictorMP/Test.java
+++ src/MarinAndVictorMP/Test.java
@@ -1,4 +1,4 @@
-package MarinAndVictorMP;
+package src.MarinAndVictorMP;
 
 public class Test {
     public static void main(String[] args) {
