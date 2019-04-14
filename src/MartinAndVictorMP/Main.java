aff8eaf68961db31a544b148fac23921834a0153
martinkarlik <mkarli18@student.aau.dk>
2019-04-14 20:54:36 +0200

    fixing the xml file

--- src/MartinAndVictorMP/Main.java
+++ src/MartinAndVictorMP/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
+
                 new GUI();
             }
         });
